37726e1cccd2fe13c20b6d6348a38b5006681ace
LeftEarEast <635774040@qq.com>
2019-08-19 16:27:55 +0800

    2019/8/19

--- zubajie/app/src/main/java/com/example/a63577/myapplication/MYActivity.java
+++ zubajie/app/src/main/java/com/example/a63577/myapplication/MYActivity.java
@@ -1,22 +1,19 @@
 package com.example.a63577.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MYActivity extends AppCompatActivity {
-private Button first_page;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-        first_page=(Button)findViewById(R.id.first_page);
-
-        first_page.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                finish();
-            }
-        });
+    }
+    public void person_mes(View v){
+        Intent intent=new Intent(MYActivity.this,authentication.class);
+        startActivity(intent);
     }
 }

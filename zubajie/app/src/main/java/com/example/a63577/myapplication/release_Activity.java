140bf7f71db8f8eb9a3653354ee3c92336067b0a
LeftEarEast <635774040@qq.com>
2019-08-20 11:23:55 +0800

    2019/8/20

--- zubajie/app/src/main/java/com/example/a63577/myapplication/release_Activity.java
+++ zubajie/app/src/main/java/com/example/a63577/myapplication/release_Activity.java
@@ -9,23 +9,33 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 
 public class release_Activity extends AppCompatActivity {
 
     Button selectPicture;
-    Bitmap bitmap;
-    String item_type;
-    String item_time;
+    Bitmap bitmap;  //图片
+
+
+    String item_type; //分类
+
+    String item_time; //有效时长
+
 
     EditText edit;
-    int day;
+
+    EditText edit_title; //标题
+    EditText edit_supply; //补充说明
+    EditText edit_price; //价格
+
     //选取图片按钮
     public void select_picture(View view) {
         Intent intent = new Intent(Intent.ACTION_PICK, null);
@@ -66,15 +76,17 @@ public class release_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_release);
 
-
-
+        //获取对应的输入框
+        edit_title = (EditText) findViewById(R.id.title);
+        edit_supply = (EditText) findViewById(R.id.supplement);
+        edit_price = (EditText) findViewById(R.id.price);
 
 
         final String[] ctype = new String[]{"全部", "资料", "电器","文具", "其他"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, ctype);  //创建一个数组适配器
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);     //设置下拉列表框的下拉选项样式
 
-        Spinner spinner = super.findViewById(R.id.spinner1);
+        final Spinner spinner = super.findViewById(R.id.spinner1);
         spinner.setAdapter(adapter);
 
         //下面获取选择的值。
@@ -85,7 +97,7 @@ public class release_Activity extends AppCompatActivity {
                 item_type = ctype[arg2];
                 //设置显示当前选择的项
                 arg0.setVisibility(View.VISIBLE);
-
+                item_type = spinner.getSelectedItem().toString(); //获取分类
             }
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
@@ -112,17 +124,35 @@ public class release_Activity extends AppCompatActivity {
                 arg0.setVisibility(View.VISIBLE);
                 if(item_time=="无限"){
                     edit=findViewById(R.id.editText4);
-                    edit.setVisibility(View.GONE);
+                    edit.setVisibility(View.GONE);;
+
+                    //无限天数
                 }
                 else{
                     edit=findViewById(R.id.editText4);
                     edit.setVisibility(View.VISIBLE);
+
+                    item_time = edit.getText().toString();  //多少天
                 }
             }
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
             }
         });
+    }
+
+    //上传订单的数据
+    public void submit(View v){
+
+
+      String  title  = edit_title.getText().toString();
+
+      String supply = edit_supply.getText().toString();
+
+      String price = edit_price.getText().toString();
+
+
+
     }
 }
 

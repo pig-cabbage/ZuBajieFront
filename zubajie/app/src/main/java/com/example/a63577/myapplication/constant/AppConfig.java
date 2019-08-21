10b21fa07db6a3fbcaebf6b48fe6d495941ee94d
pig-cabbage <2089319261@qq.com>
2019-08-21 15:51:42 +0800

    完成前后端调连

---
+++ zubajie/app/src/main/java/com/example/a63577/myapplication/constant/AppConfig.java
@@ -0,0 +1,9 @@
+package com.example.a63577.myapplication.constant;
+
+public class AppConfig {
+
+    public final static String BASE_URL_PATH="http://192.168.0.103:8080";
+    public final static String DISPLAY_ORDER = BASE_URL_PATH.concat("/displayorderbyuserid");
+    public final static String INSERT_STUDENT = BASE_URL_PATH.concat("/insert");
+    public final static String ADD_MORE_STUDENTS = BASE_URL_PATH.concat("/add_more_students");
+}
